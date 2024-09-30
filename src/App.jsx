08465a848edc80701aa716e916d70fa61599c981
Robin Ahn <rahn@giftgruen.com>
2024-09-30 09:10:02 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="bg-gray-100 h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-center mt-8 text-red-800">
+        Hello, Vite + React!
+      </h1>
+    </div>
+  )
+}
+
+export default App
